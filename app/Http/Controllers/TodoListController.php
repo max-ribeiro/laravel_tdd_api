7ceb4460121a01a241642d9881c805daf23f5b5f
max-ribeiro <max_ribeiro@live.com>
2023-08-30 20:19:38 -0300

    feat: created the first test using model

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TodoList;
 use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
+
     //
     public function index() {
+        $lists = TodoList::all();
         return response(['list' => []]);
     }
 }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,19 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @test
      */
-    public function teste_store_todo_list()
+    public function teste_get_todo_list()
     {
+        $this->withoutExceptionHandling();
+        TodoList::create(['name' => 'Max']);
+
         $response = $this->getJson('api/todo-list');
         $this->assertEquals(1, count($response->json()));
     }
